ee2552e97242daddde4fe2d84e4a7e1df051da80
DuDeel <hopkalissa@gmail.com>
2023-10-01 19:35:35 +0400

    push main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,15 @@ import 'package:crypto/repositories/crypto_coins/abstract_coins_repository.dart'
 import 'package:crypto/theme/crypto_coins_list_app.dart';
 import 'repositories/crypto_coins/crypto_coins_repository.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
       () => CryptoCoinsRepository(dio: Dio()));
 
